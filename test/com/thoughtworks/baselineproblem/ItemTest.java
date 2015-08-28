ac61dd1d8173adc99d6b516eaf839b99cdcf67db
deepika <deepikav2793@gmail.com>
2015-08-28 18:58:48 +0530

    Added a specicifcation that Line Should Not Have Sales Tax if it is a Chocolate Bar

--- test/com/thoughtworks/baselineproblem/ItemTest.java
+++ test/com/thoughtworks/baselineproblem/ItemTest.java
@@ -2,10 +2,15 @@ package com.thoughtworks.baselineproblem;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
+
 public class ItemTest {
 
     @Test
     public void shouldHaveNoSalesTaxIfChocolateBar() {
         Item item=new Item("chocolate bar", 0.85);
+
+        assertFalse(item.hasSalesTax());
     }
+
 }
